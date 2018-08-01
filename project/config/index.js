4cdd3e47c45f61b612728aef109157e0df17cacd
Yuhan zhong <715181149@qq.com>
2018-08-01 11:25:55 +0800

    Update index.js

--- project/config/index.js
+++ project/config/index.js
@@ -1,6 +1,6 @@
 'use strict'
 // Template version: 1.2.5
-// see http://vuejs-templates.github.io/webpack for documentation.
+// see http://vuejs-templates.github.io/webpack for documentat ion.
 
 
 const path = require('path')
